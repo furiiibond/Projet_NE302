fcec2da1ecade9f13996e40dce3f9b6d47674c29
jeanc <jeancamlap@gmail.com>
2022-02-23 19:03:19 +0100

    init struct and generatenodes

--- api.c
+++ api.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 #include "api.h"
+
+typedef struct node {
+    char *tag;
+    char *value; // Value of the field
+    int len;  // number of char
+    struct node *child; // point on a child node.
+    struct node *brother; // point on a brother node
+} node;
+
+
+
+node *generateNodes(char* value ,int len , char nextDelim[]){
+    node *node= malloc(sizeof(node));
+    //node->tag     calculated
+    node->len = len;
+    node->value = value;
+
+    return node;
+}
 
 int parseur(char *req, int len)
 {
