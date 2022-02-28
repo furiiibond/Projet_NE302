f33e40c2f6d722c51b90019da0ab856e57d8a059
jeanc <jeancamlap@gmail.com>
2022-02-28 13:01:58 +0100

    mains files

--- main.c
+++ main.c
@@ -1,32 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <errno.h>
+
 #include "api.h"
-/// Incluez ici les fichiers d'entête nécessaires pour l'execution de ce programme.
-/// La fonction parseur doit être dans un autre fichier .c
-/// N'ajouter aucun autre code dans ce fichier.
-int main(int argc, char *argv[])
-{
-    char req[]="GET / HTTP/1.0\r\nHost: www.google.com\r\nTransfer-Encoding: gzip\r\n\r\n";
-    if (argc != 2 ) { printf("usage: %s <rulename>\n",argv[0]); return 0; }
-    if (parseur(req,strlen(req))) {
-        _Token *r,*tok;
-        void *root ;
-        root=getRootTree();
-        r=searchTree(root,argv[1]);
-        tok=r;
+
+#define false 0
+
+int main(int argc, char *argv[]) {
+    int res, fi;
+    char *p = NULL, *addr;
+    struct stat st;
+
+    if (argc < 3 ) {
+        printf("Usage: httpparser <file> <search>\nAttention <search> is case sensitive\n");
+        exit(1);
+    }
+    /* ouverture du fichier contenant la requête */
+    if ((fi = open(argv[1], O_RDWR)) == -1) {
+        perror("open");
+        return false;
+    }
+    if (fstat(fi, &st) == -1)           /* To obtain file size */
+        return false;
+    if ((addr = mmap(NULL, st.st_size, PROT_WRITE, MAP_PRIVATE, fi, 0)) == NULL)
+        return false;
+
+    // This is a special HACK since identificateur in C can't have character '-'
+
+    if (argc == 3 ) {
+        p = argv[2];
+        printf("searching for %s\n",p);
+        while (*p) {
+            if (*p == '-') { *p = '_'; }
+            p++;
+        }
+        p = argv[2];
+    }
+    // call parser and get results.
+    if (res = parseur(addr,st.st_size)) {
+        _Token *r, *tok;
+        void *root = NULL;
+        root = getRootTree();
+        r = searchTree(root,p);
+        tok = r;
         while (tok) {
             int l;
             char *s;
-            s=getElementValue(tok->node,&l);
-            printf("FOUND [%.*s]\n",l,s);
-            tok=tok->next;
+            s = getElementValue(tok->node,&l);
+            printf("FOUND [%.*s]\n", l, s);
+            tok = tok->next;
         }
         purgeElement(&r);
-        purgeTree(root) ;
+        purgeTree(root);
     }
-    else {
-        return 0;
-    }
-    return 1;
+    close(fi);
+    return res;
 }
