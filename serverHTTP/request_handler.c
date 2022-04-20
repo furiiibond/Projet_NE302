dcceeb03c57c3022cb8a77e7e31fe86ed6095f94
AlphaGX <alphax9slot25key64@gmail.com>
2022-04-20 13:48:06 +0200

    déplacement de fonction server -> request_handler

--- serverHTTP/request_handler.c
+++ serverHTTP/request_handler.c
@@ -1,5 +1,35 @@
 #include "request_handler.h"
 
 int RequestHandler(message *requete, HTML_Rep* reponse,Fichier* file){
+	
+	if ((res=parseur(requete->buf,requete->len))) {
+			_Token *r,*tok,*root;
+
+			// get the root of the tree this is no longer opaque since we know the internal type with httpparser.h
+			//void *root;
+			writeDirectClient(requete->clientId,REPONSE,strlen(REPONSE));
+			root=getRootTree();
+			r=searchTree(root,"HTTP-message");
+			tok=r;
+			while (tok) {
+				int l;
+				char *s;
+				// node is no longer opaque
+				/* Lnode *node;
+				node=(Lnode *)tok->node; */
+				/** Utilisation de getElementValue() pour traiter le type opaque */
+				s = getElementValue(tok->node, &l);
+				writeDirectClient(requete->clientId,s,l);
+				tok=tok->next;
+			}
+			purgeElement(&r);
+		purgeTree(root);
+		} else {
+			writeDirectClient(requete->clientId,ERROR,strlen(ERROR));
+		}
+	
+	
+	
+	
 	return 0;
 }
