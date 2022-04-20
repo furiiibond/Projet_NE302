72b6f81e8a9e259504607e922a099337c7045506
Rancma987 <mathis.ranc@etu.esisar.grenoble-inp.fr>
2022-04-20 13:56:29 +0200

    Update module.image_magick.h
    Added function using <magic.h> to get MIME type of a file

---
+++ serverHTTP/module.image_magick.h
@@ -0,0 +1,29 @@
+#include <magic.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEFAULT_TYPE "text/plain"
+
+void get_file_type(const char* path, char* type){
+	const char* tmp;
+	magic_t mag;
+	/*Create magic cookie pointer*/
+	mag = magic_open(MAGIC_MIME_TYPE);	//MAGIC_MIME_TYPE will allow to return a MIME type string
+	if (mag == NULL){
+		perror("magic open");
+		strcpy(type,DEFAULT_TYPE);
+		return;
+	}
+	if (magic_load(mag, NULL) != 0) {	//Load magic database
+		printf("error loading magic database: %s\n", magic_error(mag));
+        magic_close(mag);
+		strcpy(type,DEFAULT_TYPE);
+        return;
+    }
+	/*Get MIME type*/
+	tmp = magic_file(mag, path);	//tmp is needed because magic_file returns a const char*
+	strcpy(type,tmp);
+	/*Close magic database and deallocates ressources used*/
+	magic_close(mag);
+}
